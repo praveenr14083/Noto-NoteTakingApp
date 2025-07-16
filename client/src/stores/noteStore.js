6460b4839e6037d3ab4e02fa74847a23ae9e00b7
praveenr14083 <praveenr14083@gmail.com>
2025-07-17 00:21:37 +0530

    feat: .env added

--- client/src/stores/noteStore.js
+++ client/src/stores/noteStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api/notes";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const useNoteStore = create((set, get) => ({
   notes: [],
--- server/app.js
+++ server/app.js
@@ -73,5 +73,5 @@ app.delete("/api/notes/:id", async (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
